cb0144645c3680f7bf62a592ba6f7e1c9171bbe4
serii burduja <seriiburduja@gmail.com>
2026-07-31 11:39:01 +0300

    feat: create sidebar

--- src/app/layouts/default-layout/default-layout.ts
+++ src/app/layouts/default-layout/default-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../../components/header/header';
+import { Sidebar } from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './default-layout.html',
 })
 export class DefaultLayout {
