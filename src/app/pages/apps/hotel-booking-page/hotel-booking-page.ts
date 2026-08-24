6b65fa708aacc2dcf6a46b4d44f3e980a1a104f6
serii burduja <seriiburduja@gmail.com>
2026-08-24 09:49:06 +0300

    feat: hotel booking page start header

--- src/app/pages/apps/hotel-booking-page/hotel-booking-page.ts
+++ src/app/pages/apps/hotel-booking-page/hotel-booking-page.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hotel-booking-page',
   imports: [],
   templateUrl: './hotel-booking-page.html',
 })
-export class HotelBookingPage {}
+export class HotelBookingPage {
+  availableRooms = signal<number>(0);
+  favorites = signal<number>(0);
+  searchQuery = signal<string>('');
+}
