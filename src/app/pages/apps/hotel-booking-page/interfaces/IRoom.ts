2614ee7bf4ce381c960f1471f19900f1d032f449
serii burduja <seriiburduja@gmail.com>
2026-08-24 09:30:57 +0300

    feat: room interface

---
+++ src/app/pages/apps/hotel-booking-page/interfaces/IRoom.ts
@@ -0,0 +1,10 @@
+export interface IRoom {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  capacity: number;
+  image: string;
+  available: boolean;
+  rating: number;
+}
