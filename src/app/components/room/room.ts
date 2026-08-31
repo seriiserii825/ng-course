e90b2e2d93141d91c0d3f2dc1dbf765d19d1576c
serii burduja <seriiburduja@gmail.com>
2026-08-31 09:32:10 +0300

    feat: rooms images and set variables

--- src/app/components/room/room.ts
+++ src/app/components/room/room.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { BtnShared } from '../../shared/btn-shared/btn-shared';
+import { IRoom } from '@/app/pages/apps/hotel-booking-page/interfaces/IRoom';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-room',
-  imports: [BtnShared],
+  imports: [BtnShared, NgClass],
   templateUrl: './room.html',
 })
-export class Room {}
+export class Room {
+  room = input<IRoom>();
+}
